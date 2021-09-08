c2b42d09a46aa090c1a41ec248d782df0b8354f1
Julian <colonj22@gmail.com>
2021-09-08 07:26:05 -0400

    improved vocab request error handling

--- src/main.py
+++ src/main.py
@@ -49,27 +49,18 @@ async def on_message(message):
     if len(splitMsg) == 1 and splitMsg[0] == 'cicero':
         await message.channel.send(help_msg)
 
-    elif len(splitMsg) == 2 and splitMsg[0] == 'latin' and splitMsg[1] == 'history':
-        await message.channel.send(latin_history_error)
-
     elif len(splitMsg) == 3 and splitMsg[0] == 'latin' and splitMsg[1] == 'history' and splitMsg[2] == 'wheelock':
         await message.channel.send(latin_history_wheelock)
 
     elif len(splitMsg) == 3 and splitMsg[0] == 'latin' and splitMsg[1] == 'history' and splitMsg[2] == 'wikipedia':
         await message.channel.send(latin_history_wikipedia)
 
+    elif len(splitMsg) in range(2, 4) and splitMsg[0] == 'latin' and splitMsg[1] == 'history':
+        await message.channel.send(latin_history_error)
+
     elif len(splitMsg) >= 3 and splitMsg[0] == 'chapter' and splitMsg[1].isnumeric():
-        if int(splitMsg[1]) < 1 or int(splitMsg[1]) > 40:
-            await message.channel.send(chapter_num_out_of_range_error(splitMsg[1]))
-        
-        elif len(splitMsg) == 3 and splitMsg[2] == 'vocab':
-            await message.channel.send(vocab_missing_command(splitMsg[1]))
-
-        elif len(splitMsg) == 4 and splitMsg[2] == 'vocab' and splitMsg[3] == 'test':
-            await message.channel.send(vocab_test_missing_command(splitMsg[1]))
-
         # List all vocab words with an audio file in the chapter
-        elif len(splitMsg) == 4 and splitMsg[2] == 'vocab' and splitMsg[3] == 'list':
+        if len(splitMsg) == 4 and splitMsg[2] == 'vocab' and splitMsg[3] == 'list':
             await send_vocab_list(message, splitMsg, db)
 
         # Display a random vocab word in the chapter
@@ -83,6 +74,15 @@ async def on_message(message):
         # List all vocab words in chapter in one language
         elif len(splitMsg) == 5 and splitMsg[2] == 'vocab' and splitMsg[3] == 'test' and splitMsg[4] in languages:
             await send_vocab_test(message, splitMsg, db)
+
+        elif int(splitMsg[1]) in range(1, 41):
+            await message.channel.send(chapter_num_out_of_range_error(splitMsg[1]))
+        
+        elif len(splitMsg) in range(3, 6) and splitMsg[2] == 'vocab':
+            await message.channel.send(vocab_missing_command(splitMsg[1]))
+
+        elif len(splitMsg) in range(4, 6) and splitMsg[2] == 'vocab' and splitMsg[3] == 'test':
+            await message.channel.send(vocab_test_missing_command(splitMsg[1]))
 
         elif len(splitMsg) == 3 and splitMsg[2] == 'exercises':
             await send_exercises_blank(message, splitMsg, db)
