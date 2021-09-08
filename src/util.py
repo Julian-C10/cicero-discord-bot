900859958847d7d4245c5360da268442a304388d
Julian <colonj22@gmail.com>
2021-09-08 09:58:08 -0400

    fixed all chapter vocab word search error handling

--- src/util.py
+++ src/util.py
@@ -89,7 +89,7 @@ def vocab_missing_command(num):
 def missing_specific_vocab_word_error(reconstructedMsg, num):
     f'Sorry, I couldn\'t find a vocab entry for "{reconstructedMsg}" in chapter {num}.\n' + suggest_error_message
 
-def missing_specific_vocab_word_error(reconstructedMsg):
+def missing_specific_vocab_word_all_chapters_error(reconstructedMsg):
     f'Sorry, I couldn\'t find a vocab entry for "{reconstructedMsg}" in any chapter.\n' + suggest_error_message
 
 def vocab_test_missing_command(num):
